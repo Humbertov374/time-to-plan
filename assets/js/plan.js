5f33446372a6e52f387eac93b90a03d7700183a6
Humbertov374 <humbertov374@gmail.com>
2022-11-28 16:28:13 -0700

    Plugins that add properties should also add the key here (null value), used to PROPERLY clone

--- assets/js/plan.js
+++ assets/js/plan.js
@@ -75,8 +75,7 @@ colorcoding();
         return hookCallback.apply(null, arguments);
     }
 
-    // This is done to register the method called with moment()
-    // without creating circular dependencies.
+    // This is done to register the method called with moment()- without creating circular dependencies.
     function setHookCallback (callback) {
         hookCallback = callback;
     }
@@ -86,8 +85,7 @@ colorcoding();
     } 
     
     function isObject(input) {
-        // IE8 will treat undefined and null as object if it wasn't for
-        // input != null
+        // IE8 will treat undefined and null as an object
         return input != null && Object.prototype.toString.call(input) === '[object Object]';
     }
 
@@ -240,4 +238,54 @@ colorcoding();
         }
 
         return m;
+    }
+
+    // Plugins that add properties should also add the key here (null value), used to PROPERLY clone.
+    var momentProperties = hooks.momentProperties = [];
+
+    function copyConfig(to, from) {
+        var i, prop, val;
+
+        if (!isUndefined(from._isAMomentObject)) {
+            to._isAMomentObject = from._isAMomentObject;
+        }
+        if (!isUndefined(from._i)) {
+            to._i = from._i;
+        }
+        if (!isUndefined(from._f)) {
+            to._f = from._f;
+        }
+        if (!isUndefined(from._l)) {
+            to._l = from._l;
+        }
+        if (!isUndefined(from._strict)) {
+            to._strict = from._strict;
+        }
+        if (!isUndefined(from._tzm)) {
+            to._tzm = from._tzm;
+        }
+        if (!isUndefined(from._isUTC)) {
+            to._isUTC = from._isUTC;
+        }
+        if (!isUndefined(from._offset)) {
+            to._offset = from._offset;
+        }
+        if (!isUndefined(from._pf)) {
+            to._pf = getParsingFlags(from);
+        }
+        if (!isUndefined(from._locale)) {
+            to._locale = from._locale;
+        }
+
+        if (momentProperties.length > 0) {
+            for (i = 0; i < momentProperties.length; i++) {
+                prop = momentProperties[i];
+                val = from[prop];
+                if (!isUndefined(val)) {
+                    to[prop] = val;
+                }
+            }
+        }
+
+        return to;
     }
